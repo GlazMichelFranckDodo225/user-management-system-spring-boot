368ccbc5a9174fd21b3a0e7fd5235b7690bd6f3d
FranckDodo91 <devlogappdgmf@gmail.com>
2023-06-18 06:53:58 +0200

    Project Initialization

---
+++ src/test/java/com/dgmf/umsspbt/UserManagementSystemSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf.umsspbt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementSystemSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
